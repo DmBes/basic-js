b992a5de8a52f3ab6c00bcc24932a006c6ad134e
BesDm <b-192@tut.by>
2022-05-02 01:02:14 +0300

    carbon

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,21 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
-  if (arr[0] == '--double-prev' || arr[0] == '--discard-prev' ){
-    arr.splice(0,1);
-  }
-  if (arr[arr.length-1] == '--discard-next' || arr[arr.length-1] == '--double-next'){
-    arr.splice(arr.length-1,0)
-  }
-  arr.forEach((value, index, array) => {
-    if (value == '--double-prev') arr[index] == arr[index - 1];
-    else if (value == '--discard-prev') arr.splice(index-1) ;
-    else if (value ==  '--discard-next') arr.splice(index,2);
-    else if (value == '--double-next') arr[index] == arr[index+1];
 
-  }
-  )
+function transform(arr) {
+    let array =arr;
+
+
+    //arr.forEach((item) => array.push(item))
+    if (!Array.isArray(array)){
+        throw new Error("'arr' parameter must be an instance of the Array!")
+    }
+    if (arr.length == 0) return array;
+    if (arr[0] == '--double-prev' || arr[0] == '--discard-prev' ){
+        arr.splice(0,1);
+    }
+    if (array[array.length-1] == '--discard-next' || array[array.length-1] == '--double-next'){
+        array.splice(array.length-1,0)
+    }
+    arr.forEach((value, index, array1) => {
+            if (value == '--double-prev') array[index] == array[index - 1];
+            else if (value == '--discard-prev') array.splice(index-1) ;
+            else if (value ==  '--discard-next') array.splice(index,2);
+            else if (value == '--double-next') array[index] == array[index+1];
+        }
+    )
+
+
+    return arr = array;
 }
 
 module.exports = {
