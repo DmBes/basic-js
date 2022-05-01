06d3ddded6d4fcf1d92a084626271c267247a924
BesDm <b-192@tut.by>
2022-05-02 01:33:19 +0300

    getCommonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const {stringify} = require("mocha/lib/utils");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,9 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  if (s1.length == 0 || s2.length == 0) return 0;
+  let count = 0;
+  for (let i = 0; i < s1.length ; i++) {
+
+
+
+    if (s2.indexOf(s1[i])>=0)
+    {
+      count++;
+      s2 = s2.replace(s1[i],'');
+    }
+
+  }
+
+  return count;
+
+
 }
 
 module.exports = {
